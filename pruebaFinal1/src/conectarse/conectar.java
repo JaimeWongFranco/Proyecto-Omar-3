1abe81ed87108d6e502873cecce6d22b220a0e3a
FranciscoVz <34484656+FranciscoVz@users.noreply.github.com>
2017-12-18 12:34:00 -0500

    asd

--- pruebaFinal1/src/conectarse/conectar.java
+++ pruebaFinal1/src/conectarse/conectar.java
@@ -1,5 +1,25 @@
 package conectarse;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 public class conectar {
+	
+		Connection cc = null;
+		
+		public Connection conexion()
+		{
+			try {
+				Class.forName("com.mysql.jdbc.Driver");
+				cc=DriverManager.getConnection("jdbc:mysql://localhost/supermercado","root","");
+				
+				
+			} catch (ClassNotFoundException | SQLException e) {
+				
+				e.printStackTrace();
+			}
+			return cc;
+		}
+		
 
+	
 }
